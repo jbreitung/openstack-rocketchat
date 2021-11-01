f4b26ef55603c40ec39cb7247f0524e15ac81ac2
Julian Breitung <julian.breitung@informatik.hs-fulda.de>
2021-11-01 17:24:12 +0100

    Initial commit

---
+++ scripts/backup/cloud-init.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+apt-get update
+apt-get -y install nginx
+rm /var/www/html/index.html
+cat > /var/www/html/index.html << EOF
+<!DOCTYPE html>
+<html>
+  <body>
+    <h1>Greetings from my Cloud-Init :-)</h1>
+    <p>hostname</p>
+  </body>
+</html>
+EOF
+sed -i "s/hostname/$HOSTNAME/" /var/www/html/index.html
+sed -i "1s/$/ $HOSTNAME/" /etc/hosts
