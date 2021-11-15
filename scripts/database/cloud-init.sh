e1d5590e540b389a2eb62d1e1303627b858760f9
Julian Breitung <julian.breitung@informatik.hs-fulda.de>
2021-11-15 18:33:07 +0100

    Basic installation scripts for web and database hosts.

--- scripts/database/cloud-init.sh
+++ scripts/database/cloud-init.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
-apt-get update
-wget -qO - https://www.mongodb.org/static/pgp/server-5.0.asc | sudo apt-key add -
-touch /etc/apt/sources.list.d/mongodb-org-5.0.list
-echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/5.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-5.0.list
-apt-get update
-apt-get install -y mongodb-org
+
+apt update
+apt upgrade -y
+
+apt install -y --no-install-recommends ca-certificates curl wget gnupg dirmngr xz-utils
+
+curl -fsSL https://www.mongodb.org/static/pgp/server-4.4.asc | sudo apt-key add -
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/4.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.4.list
+
+apt update
+apt install -y mongodb-org
+
+mv ./mongod.conf /etc/mongod.conf
+
+systemctl stop mongod.service
+systemctl start mongod.service
+systemctl enable mongod
+
+mongo --eval "rs.initiate({
+              _id: 'rs0',
+              members: [ { _id: 0, host: 'localhost:27017' } ]})"
+			  
--- scripts/web/cloud-init.sh
+++ scripts/web/cloud-init.sh
@@ -1,7 +1,41 @@
 #!/bin/bash
+NODE_ENV=production
+NODE_VERSION=12.22.7
+ROCKETCHAT_VERSION=4.0.5
+ARCH=x64
+
 apt update
-snap install rocketchat-server
-snap set rocketchat-server port=80
-snap set rocketchat-server mongo-url=mongodb://localhost:27017/savannasaurus
-snap set rocketchat-server mongo-oplog-url=mongodb://localhost:27017/local
-systemctl restart snap.rocketchat-server.rocketchat-server.service
+apt upgrade -y
+
+apt install -y --no-install-recommends ca-certificates curl wget gnupg dirmngr xz-utils
+
+curl -fsSLO --compressed "https://nodejs.org/dist/v$NODE_VERSION/node-v$NODE_VERSION-linux-$ARCH.tar.xz"
+tar -xJf "node-v$NODE_VERSION-linux-$ARCH.tar.xz" -C /usr/local --strip-components=1 --no-same-owner
+rm "node-v$NODE_VERSION-linux-$ARCH.tar.xz"
+
+groupadd -r rocketchat
+useradd -r -g rocketchat rocketchat
+mkdir -p /app/uploads
+chown rocketchat:rocketchat /app/uploads
+
+apt install -y --no-install-recommends fontconfig
+apt install -y --no-install-recommends g++ make python ca-certificates curl gnupg
+
+curl -fSL "https://releases.rocket.chat/${ROCKETCHAT_VERSION}/download" -o rocket.chat.tgz
+tar zxf rocket.chat.tgz
+rm rocket.chat.tgz
+
+cd bundle/programs/server
+npm install
+npm cache clear --force
+
+chown -R rocketchat:rocketchat /app
+
+mkdir -p /opt/RocketChat
+cd ../../../
+mv bundle/ /opt/RocketChat/
+cd /opt/RocketChat/
+
+source ./openstack.sh
+
+//Openstack API abfragen nach Loadbalancer für ROOT_URL und so.
